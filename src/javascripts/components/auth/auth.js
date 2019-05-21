c708463b2c7f76be35f1aeb9eae0650cd02a7d1f
Keith Walker <Keithrwalker418@gmail.com>
2019-05-20 20:51:58 -0500

    login/reg

--- src/javascripts/components/auth/auth.js
+++ src/javascripts/components/auth/auth.js
@@ -1,8 +1,21 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
 import util from '../../helpers/util';
 
+import loginBtn from './loginBtn.png';
+
+const signMeIn = () => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider);
+};
+
 const authBuild = () => {
-  const auth = '<h1>auth</h1>';
+  let auth = '<button class="btn google-auth" id="google-auth">';
+  auth += `<img src="${loginBtn}">`;
+  auth += '</button>';
   util.printToDom('auth', auth);
+  document.getElementById('google-auth').addEventListener('click', signMeIn);
 };
 
 export default { authBuild };
