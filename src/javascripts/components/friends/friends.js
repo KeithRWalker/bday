66895f6da1ef815b7f6ccdf1d59c6152b6df32ec
Keith Walker <Keithrwalker418@gmail.com>
2019-06-01 10:08:24 -0500

    WIP

--- src/javascripts/components/friends/friends.js
+++ src/javascripts/components/friends/friends.js
@@ -28,10 +28,28 @@ const newFriendButton = () => {
   document.getElementById('saveNewFriend').addEventListener('click', createNewFriend);
 };
 
-const showFriends = () => {
-  const domString = '<button class="btn btn-danger" id="add-friend-btn">Add Friend</button>';
-  util.printToDom('bday', domString);
+const showFriends = (friends) => {
+  let friendsSt = '<button class="btn btn-danger" id="add-friend-btn">Add Friend</button>';
+  friends.forEach((friend) => {
+    friendsSt += `<h1>${friend.name}</h1>`;
+  });
+  util.printToDom('friends-table', friendsSt);
+  util.printToDom('friends-table');
   document.getElementById('add-friend-btn').addEventListener('click', newFriendButton);
 };
 
-export default { showFriends };
+const getFriends = (uid) => {
+  friendsData.getFriendsByUid(uid)
+    .then((friends) => {
+      console.error('friends loaded from "getFriends', friends);
+      showFriends(friends);
+      document.getElementById('name').value = '';
+      document.getElementById('email').value = '';
+      document.getElementById('birthday').classList.remove('hide');
+      document.getElementById('new-friend').classList.add('hide');
+      getFriends(firebase.auth().currentUser.uid); // eslint-disable-line no-use-before-define
+    })
+    .catch(err => console.error(err, 'error from "getFriends" in friends.js'));
+};
+
+export default { showFriends, getFriends };
--- src/javascripts/helpers/data/friendsData.js
+++ src/javascripts/helpers/data/friendsData.js
@@ -6,4 +6,17 @@ const firebaseUrl = apiKeys.firebaseKeys.databaseURL;
 
 const addNewFriend = friendObject => Axios.post(`${firebaseUrl}/friends.json`, friendObject);
 
-export default { addNewFriend };
+const getFriendsByUid = uid => new Promise((resolve, reject) => {
+  Axios.get(`${firebaseUrl}/friends.json?orderBy="uid"&equalTo="${uid}"`)
+    .then((results) => {
+      const friendResults = results.data;
+      const friends = [];
+      Object.keys(friendResults).forEach((friendId) => {
+        friendResults[friendId].id = friendId;
+        friends.push(friendResults[friendId]);
+      });
+      resolve(friends);
+    }).catch(err => reject(err));
+});
+
+export default { addNewFriend, getFriendsByUid };
